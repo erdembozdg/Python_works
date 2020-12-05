3d50b721fdbc3bd8607b49f35246a0e569513b16
Erdem Bozdag <erdembozdg@gmail.com>
2020-12-05 11:37:05 -0800

    Updated hacker-rank

--- python-interview/intermediate/recursive.py
+++ python-interview/intermediate/recursive.py
@@ -23,3 +23,14 @@ def word_split(phrase,list_of_words, output = None):
     return output
 print(word_split('themanran',['the','ran','man']))
 print(word_split('ilovedogsJohn',['i','am','a','dogs','lover','love','John']))
+
+# staircase problem
+cache = dict()
+def stepPerms(n):
+    if n == 1:return 1
+    if n == 2:return 2
+    if n == 3:return 4
+    if n not in cache:
+        cache[n] = stepPerms(n-1)+stepPerms(n-2)+stepPerms(n-3)
+    return cache[n]
+print(stepPerms(10))
