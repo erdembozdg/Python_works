189896ec90403a048316bfdbcf38058996e3e43f
Erdem Bozdag <erdembozdg@gmail.com>
2021-02-15 21:04:26 -0800

    fixed

--- go/projects/banking/main.go
+++ go/projects/banking/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "https://github.com/erdembozdg/coding/tree/main/go/rest_api/banking/app"
+import "github.com/erdembozdg/coding/tree/main/go/rest_api/banking/app"
 
 func main() {
 	app.Start()
